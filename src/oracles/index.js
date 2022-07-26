79c8dbcce10aa1f02f36f49bf672f05b5c5b4996
codeoneline <mutouji1@163.com>
2022-07-26 11:17:57 +0800

    add configuration

--- src/oracles/index.js
+++ src/oracles/index.js
@@ -29,26 +29,36 @@ class Oracle extends Component {
   }
 
   render() {
-    let prices = <div>Loading...</div>;
-    let configs = <div>Loading...</div>;
-    let sgs = <div>Loading...</div>;
+    const keys = Object.keys(this.state)
+    const subs = keys.map(key => {
+      if (this.state[key].length > 0) {
+        return <Fields title={this.state[key].title} columns={this.state[key].columns} data={this.state[key].data} />
+      } else {
+        return <div>Loading...</div>
+      }
+    })
+    // let prices = <div>Loading...</div>;
+    // let configs = <div>Loading...</div>;
+    // let sgs = <div>Loading...</div>;
   
-    if (this.state.prices) {
-      prices = <Fields title={this.state.prices.title} columns={this.state.prices.columns} data={this.state.prices.data} />
-    }
-    if (this.state.configs) {
-      configs = <Fields title={this.state.configs.title} columns={this.state.configs.columns} data={this.state.configs.data} />
-    }
-    if (this.state.sgs && this.state.sgs.length > 0) {
-      sgs = this.state.sgs.map(sg => {
-        return <Fields title={sg.title} columns={sg.columns} data={sg.data} />
-      })
-    }
+    // if (this.state.prices) {
+    //   prices = <Fields title={this.state.prices.title} columns={this.state.prices.columns} data={this.state.prices.data} />
+    // }
+    // if (this.state.configs) {
+    //   configs = <Fields title={this.state.configs.title} columns={this.state.configs.columns} data={this.state.configs.data} />
+    // }
+    // if (this.state.sgs && this.state.sgs.length > 0) {
+    //   sgs = this.state.sgs.map(sg => {
+    //     return <Fields title={sg.title} columns={sg.columns} data={sg.data} />
+    //   })
+    // }
     return (
       <div className='oracles'>
-      { prices }
-      { configs }
-      { sgs }
+        {/* { prices }
+        { configs }
+        { sgs } */
+        ...subs
+      }
       </div>
     )
   }
