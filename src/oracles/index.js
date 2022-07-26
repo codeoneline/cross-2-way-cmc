bea0f0ae41ceaac2b63a4e51d4504e7ae0c18d3e
codeoneline <mutouji1@163.com>
2022-07-26 13:55:33 +0800

    remove log

--- src/oracles/index.js
+++ src/oracles/index.js
@@ -29,41 +29,39 @@ class Oracle extends Component {
   }
 
   render() {
-    const keys = Object.keys(this.state)
-    // console.log(`keys are ${JSON.stringify(keys, null, 2)}`)
-    let subs = <div>Loading...</div>
-    if (keys.length > 0) {
-      subs = keys.map(key => {
-        // console.log(`${key} states are ${JSON.stringify(this.state[key], null, 2)}`)
-        if (this.state[key].columns) {
-          return <Fields title={this.state[key].title} columns={this.state[key].columns} data={this.state[key].data} />
-        } else {
-          return <div>Loading...</div>
-        }
-      })
-    }
-    // let prices = <div>Loading...</div>;
-    // let configs = <div>Loading...</div>;
-    // let sgs = <div>Loading...</div>;
-  
-    // if (this.state.prices) {
-    //   prices = <Fields title={this.state.prices.title} columns={this.state.prices.columns} data={this.state.prices.data} />
-    // }
-    // if (this.state.configs) {
-    //   configs = <Fields title={this.state.configs.title} columns={this.state.configs.columns} data={this.state.configs.data} />
-    // }
-    // if (this.state.sgs && this.state.sgs.length > 0) {
-    //   sgs = this.state.sgs.map(sg => {
-    //     return <Fields title={sg.title} columns={sg.columns} data={sg.data} />
+    // const keys = Object.keys(this.state)
+    // // console.log(`keys are ${JSON.stringify(keys, null, 2)}`)
+    // let subs = <div>Loading...</div>
+    // if (keys.length > 0) {
+    //   subs = keys.map(key => {
+    //     // console.log(`${key} states are ${JSON.stringify(this.state[key], null, 2)}`)
+    //     if (this.state[key].columns) {
+    //       return <Fields title={this.state[key].title} columns={this.state[key].columns} data={this.state[key].data} />
+    //     } else {
+    //       return <div>Loading...</div>
+    //     }
     //   })
     // }
+    let prices = <div>Loading...</div>;
+    let configs = <div>Loading...</div>;
+    let sgs = <div>Loading...</div>;
+  
+    if (this.state.prices) {
+      prices = <Fields title={this.state.prices.title} columns={this.state.prices.columns} data={this.state.prices.data} />
+    }
+    if (this.state.configs) {
+      configs = <Fields title={this.state.configs.title} columns={this.state.configs.columns} data={this.state.configs.data} />
+    }
+    if (this.state.sgs && this.state.sgs.length > 0) {
+      sgs = this.state.sgs.map(sg => {
+        return <Fields title={sg.title} columns={sg.columns} data={sg.data} />
+      })
+    }
     return (
       <div className='oracles'>
-        {/* { prices }
+        { prices }
         { configs }
-        { sgs } */
-        subs
-      }
+        { sgs }
       </div>
     )
   }
