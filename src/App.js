f626710961cc3a88448b007af3ae1b2d88b945e3
codeoneline <mutouji1@163.com>
2022-10-18 15:04:55 +0800

    add refresh button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Chains from './chains';
 import Oracles from './oracles';
 import TokenManagers from './tokenManagers';
+import chainState from '../utils/chain-state'
 
 class App extends Component {
+
+  refreshTokenPairs = (e) => {
+    e.preventDefault();
+    chainState.refreshTokenPairs();
+    alert('refreshTokenPairs success!');
+  }
+
   render() {
     return (
       <>
         <div className='app'>
           <div className='title'>
             <div>cross chain oracle tokenManager contract states</div>
+            <button onClick={ this.refreshTokenPairs }>tokenPair的数量没变, 但内容变时, 请手动更新!</button>
           </div>
         <div className='main'>
           <Chains />
